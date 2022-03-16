2992cf83f58f473220f8136088d31d30545189a9
Crunck78 <crunck78@googlemail.com>
2022-03-16 13:35:31 +0100

    added postgres

--- join/join/settings.py
+++ join/join/settings.py
@@ -93,12 +93,26 @@ WSGI_APPLICATION = 'join.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'iybjzhzamrkdrb',
+        'PASSWORD' : '82f9940aca9daed5f2aa8fdbdcb54669749f2aa2e307c7969f5d7494094cdea7',
+        'HOST' : 'ec2-99-81-177-233.eu-west-1.compute.amazonaws.com',
+        'PORT' : '5432',
+        'NAME' : 'd7l46co23rhl8m',
+        # Heroku postgres://iybjzhzamrkdrb:82f9940aca9daed5f2aa8fdbdcb54669749f2aa2e307c7969f5d7494094cdea7@ec2-99-81-177-233.eu-west-1.compute.amazonaws.com:5432/d7l46co23rhl8m
     }
+    
 }
+
 
 
 # Password validation
